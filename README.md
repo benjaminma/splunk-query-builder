27ee23238fddd5774070139de67d0d76dcfc1f55
benjaminma <github.com.site@benjaminma.com>
2021-08-03 17:03:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# splunk-query-builder
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/splunk-query-builder)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `Hello World!`;
+
+const searchInput = document.getElementById('search-input');
+const copyButton = document.getElementById('copy-button');
+copyButton.addEventListener('click', () => {});
